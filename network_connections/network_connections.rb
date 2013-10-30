ff3e2d7750472255120f52a350b889f665da551b
AJ <sevaine@gmail.com>
2013-10-30 12:19:41 +1100

    Refactor for stats on a per-local IP basis also

--- network_connections/network_connections.rb
+++ network_connections/network_connections.rb
@@ -14,32 +14,42 @@ class NetworkConnections < Scout::Plugin
       option(:port).split(/[, ]+/).each { |port| port_hash[port.to_i] = 0 }
     end
 
-    lines = shell("netstat -n").split("\n")
+    localip_hash = {}
+    localips = shell("/sbin/ifconfig -a | awk -F':' '/inet addr/{print $2}' | grep -v '127.0.0.1' | cut -d' ' -f1").split("\n").each { |ip| localip_hash[ip] = port_hash }
+
     connections_hash = {:tcp => 0,
                         :udp => 0,
                         :unix => 0,
                         :total => 0}
 
-    lines.each { |line|
-      line = line.squeeze(" ").split(" ")
-      next unless line[0] =~ /tcp|udp|unix/
-      connections_hash[:total] += 1
-      protocol = line[0].sub(/\d+/,'').to_sym
-      connections_hash[protocol] += 1 if connections_hash[protocol]
+    connections_hash[:tcp]    = shell("netstat -an | awk '/tcp.*ESTAB/{count++} END {print count}'").strip
+    connections_hash[:udp]    = shell("netstat -an | awk '/udp.*ESTAB/{count++} END {print count}'").strip
+    connections_hash[:unix]   = shell("netstat -an | awk '/unix.*ESTAB/{count++} END {print count}'").strip
+    connections_hash[:total]  = shell("netstat -an | awk '/.*ESTAB/{count++} END {print count}'").strip
 
-      local_address = line[3].sub("::ffff:","") # indicates ip6 - remove so regex works
-      port = local_address.split(":")[1].to_i
-      port_hash[port] += 1 if port_hash.has_key?(port)
-    }
+    shell("netstat -an | awk '/ESTAB/{print $4}'").strip.split("\n").each do |ip_and_port|
+      ipaddress = ip_and_port.split(/:/)[0]
+      port      = ip_and_port.split(/:/)[1].to_i
+
+      if port_hash.has_key?(port)
+	port_hash[port] += 1
+	localip_hash[ipaddress][port] += 1 unless ipaddress =~ /127.0.0./
+      end
+    end    
 
     connections_hash.each_pair { |conn_type, counter|
-      report_hash[conn_type]=counter
+      report_hash["Total conn_type"]=counter
     }
 
     port_hash.each_pair { |port, counter|
       report_hash["Port #{port}"] = counter
     }
 
+    localip_hash.each_pair { |ipaddress, data| 
+      data.each_pair { |k, v|
+        report_hash["IP #{ipaddress} port #{k}"] = v if ipaddress
+      }
+    }
     report(report_hash)
   end
 

c4ed65bb9e46c1f83736d3f8bd5f9a346d5fc08d
Pete Johns <pete.johns@envato.com>
2014-09-26 12:20:00 +1000

    Whitespace

--- delayed_job/monitor_delayed_jobs.rb
+++ delayed_job/monitor_delayed_jobs.rb
@@ -2,7 +2,7 @@ $VERBOSE=false
 
 class MonitorDelayedJobs < Scout::Plugin
   ONE_DAY    = 60 * 60 * 24
-  
+
   OPTIONS=<<-EOS
   path_to_app:
     name: Full Path to the Rails Application
@@ -11,35 +11,35 @@ class MonitorDelayedJobs < Scout::Plugin
     name: Rails environment that should be used
     default: production
   EOS
-  
+
   needs 'active_record', 'yaml', 'erb'
 
   # IMPORTANT! Requiring Rubygems is NOT a best practice. See http://scoutapp.com/info/creating_a_plugin#libraries
   # This plugin is an exception because we to subclass ActiveRecord::Base before the plugin's build_report method is run.
-  require 'rubygems' 
+  require 'rubygems'
   require 'active_record'
   class DelayedJob < ActiveRecord::Base; end
   DelayedJob.default_timezone = :utc
 
   def build_report
     app_path = option(:path_to_app)
-    
+
     # Ensure path to db config provided
     if !app_path or app_path.empty?
       return error("The path to the Rails Application wasn't provided.","Please provide the full path to the Rails Application (ie - /var/www/apps/APP_NAME/current)")
     end
-    
+
     db_config_path = app_path + '/config/database.yml'
-    
+
     if !File.exist?(db_config_path)
       return error("The database config file could not be found.", "The database config file could not be found at: #{db_config_path}. Please ensure the path to the Rails Application is correct.")
     end
-    
+
     db_config = YAML::load(ERB.new(File.read(db_config_path)).result)
     ActiveRecord::Base.establish_connection(db_config[option(:rails_env)])
-        
+
     report_hash = Hash.new
-    
+
     # ALl jobs
     report_hash[:total]     = DelayedJob.count
     # Jobs that are currently being run by workers
@@ -54,14 +54,14 @@ class MonitorDelayedJobs < Scout::Plugin
     report_hash[:failed]    = DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
     report_hash[:overdue] = DelayedJob.count(:conditions => ['attempts = 0 AND locked_at IS NULL and run_at < ?', Time.now.utc])
     report_hash[:erroring] = DelayedJob.count(conditions => 'last_error IS NOT NULL AND failed_at IS NULL')
-    
+
     # The oldest job that hasn't yet been run, in minutes
     if oldest = DelayedJob.find(:first, :conditions => [ 'run_at <= ? AND locked_at IS NULL AND attempts = 0', Time.now.utc ], :order => :run_at)
       report_hash[:oldest] = (Time.now.utc - oldest.run_at) / 60
     else
       report_hash[:oldest] = 0
     end
-    
+
     report(report_hash)
   end
 end

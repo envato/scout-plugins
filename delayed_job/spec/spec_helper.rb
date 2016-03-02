091a2b0fa3ae827934f117a7eebbc9c4789535f8
Patrick Robinson <patrick.robinson@envato.com>
2016-03-02 17:44:30 +1100

    Use the same database path as the tests

--- delayed_job/spec/spec_helper.rb
+++ delayed_job/spec/spec_helper.rb
@@ -7,7 +7,7 @@ require File.dirname(__FILE__) + '/../monitor_delayed_jobs'
 RAILS_ROOT = File.dirname(__FILE__) + '/rails'
 
 FileUtils.rm_rf RAILS_ROOT + '/db/test.sqlite3'
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => RAILS_ROOT + '/db/test.sqlite3'
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => 'db/test.sqlite3'
 
 class CreateDelayedJobs < ActiveRecord::Migration
   def self.up
